a1d2cabba2b67d2341a55ea5f63bb79d109a501a
JoseGodinez2021 <67968966+JoseGodinez2021@users.noreply.github.com>
2020-08-10 22:46:38 -0500

    new File

---
+++ Jose_G_Tab/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Jose_G_Tab
+//
+//  Created by José Godinez on 8/6/20.
+//  Copyright © 2020 José Godinez. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class viewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
